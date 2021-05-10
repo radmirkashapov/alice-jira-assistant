825382836d3c2a51ff15dd97c072b55c8a387e80
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2021-05-10 18:11:15 +0300

    Added logs

--- src/controllers/alice.controller.js
+++ src/controllers/alice.controller.js
@@ -4,6 +4,7 @@ const micro = require('micro')
 
 exports.aliceEndpoint = async(req, reply) => {
     try {
+        console.log(req)
         const { request, session, state } = await micro.json(req);
         const sessionState = state && state.session || {};
         const response = session.new
