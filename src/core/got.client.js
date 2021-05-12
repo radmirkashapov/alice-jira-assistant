4123272e6b470d1283df9cfacaa8d47dda6585da
Radmir Kashapov <radmir.kashapov.150700@gmail.com>
2021-05-12 11:51:02 +0300

    Token fix

--- src/core/got.client.js
+++ src/core/got.client.js
@@ -6,7 +6,7 @@ exports.client = got.extend({
     hooks: {
         beforeRequest: [
             options => {
-                options.headers['Authorization'] = 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMjUiLCJlbWFpbCI6InJrYXNoYXBvdkBza29sb3BlbmRyYS5jb20iLCJyb2wiOlsiUk9MRV9VU0VSIl0sImlzcyI6Imh5YnJpZC5sb2NhbGhvc3QiLCJpYXQiOjE2MjA2NDY0NjUsImV4cCI6MTYyMDczMjg2NX0.Mg1e5PV_xHq3gMNI-j-xHMzF4seOKwKZXnR8uD4dpZmeHVJCGL6X3YW-9YOeJBBoTTS27maapPyPqUjgG8-n8Ed0GdtBQf8Hu2G8ES844Mgq8YjwKrt994OZP6vnhm8_9W6rBM9honO-8gYJ_Lbv7RYNLYwspWSWr4PGuSaRwKsSE33pg4f7M9ik_q54VJZnOVBCn1qwc4LvOw9r38G7Dsv3XY_gOI_u1eS2Ky8ZI3BXZ29BPolrZefjb28iJz5e5vFmZkhHhiGpfwUvaxZJuCL_O_oQW1wkBDlpxMybC8CcBg2BBhBxEj_ZVbFIRAegsaOTPy5wkwX-Wis_rfFERQ';
+                options.headers['Authorization'] = 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIxMjUiLCJlbWFpbCI6InJrYXNoYXBvdkBza29sb3BlbmRyYS5jb20iLCJyb2wiOlsiUk9MRV9VU0VSIl0sImlzcyI6Imh5YnJpZC5sb2NhbGhvc3QiLCJpYXQiOjE2MjA4MDk0MjYsImV4cCI6MTYyMDg5NTgyNn0.UFVAG8pkhwjX-iUh_-4tZmseWx0M-2fCdcCcoFHbA0hSW3w1TkcVt--GmfxsPJiR8yGf-jn-x7wZPDZKUv7KB7M_rlzje69s7Z_zenbuZS966gTmU_pZ9f_tlovL6P-cIlrEVrEZWSwg0q4l3iimll8cQu78BfCKsHFwReRwN2XKRJFdXZ6Cu8rPBrejk0VdjOv8P65dbjnLPNksn1YhkyEzrtNeXPfJ2yGKoGWfqMn-2I_A8IK49YwdxHDjDBAV6U_OGYD7LpI6Y4JhgqVkXTJBLp5iXJAOrGrlrRYrppeBALCY84RTiO3XgyNliNq-uwfXRtUEFhxlTeg1vO3wjQ';
             }
         ]
     }
